a3a2a5fbc3e224298fb2667b1067a5aa118411a9
Quoc Huy <pqhuy0211@gmail.com>
2021-07-20 12:50:02 +0700

    update userRouter

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,15 +1,20 @@
 import express from "express";
 import bcrypt from "bcryptjs";
+import expressAsyncHandler from "express-async-handler";
 import data from "../data.js";
 import User from "../models/userModel.js";
 import { generateToken } from "../ultils.js";
 
 const userRouter = express.Router();
 
-userRouter.get("/seed", async (req, res) => {
-  const createUser = await User.insertMany(data.users);
-  res.send({ createUser });
-});
+serRouter.get(
+  "/seed",
+  expressAsyncHandler(async (req, res) => {
+    // await User.remove({});
+    const createdUsers = await User.insertMany(data.users);
+    res.send({ createdUsers });
+  })
+);
 
 userRouter.post("/login", async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
